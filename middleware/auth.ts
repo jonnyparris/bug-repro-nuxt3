03a12186f388ff627ea76083a823b9e098ae866e
jonnyparris <6400000+jonnyparris@users.noreply.github.com>
2022-08-10 23:24:26 +0100

    use supabase again for middleware check

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,6 +1,5 @@
 export default defineNuxtRouteMiddleware(async (_to, _from) => {
-  // const user = useSupabaseUser()
-  const user = { value: false }
+  const user = useSupabaseUser()
   if (!user.value) {
     return await navigateTo('/somewhere-else')
   }
