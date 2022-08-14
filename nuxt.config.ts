e4ae3625706f8711400620eef6a9e7427f135cd9
jonnyparris <6400000+jonnyparris@users.noreply.github.com>
2022-08-14 12:22:19 +0100

    transpile websocket dep froms supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   target: 'static',
+  build: {
+    transpile: ['websocket'],
+  },
   buildModules: ['@nuxtjs/tailwindcss'],
   modules: ['@nuxtjs/supabase'],
   supabase: {
